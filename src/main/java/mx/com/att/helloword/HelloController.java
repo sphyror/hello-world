3cb09a0e4eea7f89c7a65d71cb568ddaa7ec2aa8
sphyror <sphyror@hotmail.com>
2021-05-22 21:51:40 -0500

    Tomcat embedded removed

---
+++ src/main/java/mx/com/att/helloword/HelloController.java
@@ -0,0 +1,18 @@
+package mx.com.att.helloword;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet( "/" )
+public class HelloController extends HttpServlet {
+
+    @Override
+    protected void doGet( HttpServletRequest request, HttpServletResponse response ) throws IOException {
+        PrintWriter out = response.getWriter();
+        out.print( "Hello, World!" );
+    }
+}
